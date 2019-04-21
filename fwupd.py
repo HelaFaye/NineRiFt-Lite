4bf32d1c4fbe72b36cb673d17c54122b046d7b91
Slinger360 <dilsha21@gmail.com>
2019-04-20 23:10:52 -0500

    tidied

--- fwupd.py
+++ fwupd.py
@@ -1,3 +1,4 @@
+
 from py9b.link.base import LinkOpenException, LinkTimeoutException
 from py9b.transport.base import BaseTransport as BT
 from py9b.transport.xiaomi import XiaomiTransport
@@ -5,135 +6,136 @@ from py9b.transport.ninebot import NinebotTransport
 from py9b.command.regio import ReadRegs, WriteRegs
 from py9b.command.update import *
 
+
 class FWUpd():
-	def __init__(self):
-		self.PING_RETRIES = 20
-		self.devices = {'ble': BT.BLE, 'esc': BT.ESC, 'bms': BT.BMS, 'extbms': BT.EXTBMS}
-		self.protocols = {'xiaomi': XiaomiTransport, 'ninebot': NinebotTransport}
-		self.device = ''
-		self.fwfilep = ''
-		self.interface = 'bleandroid'
-		self.protocol = 'ninebot'
-		self.address = ''
+    def __init__(self):
+        self.PING_RETRIES = 20
+        self.devices = {'ble': BT.BLE, 'esc': BT.ESC, 'bms': BT.BMS, 'extbms': BT.EXTBMS}
+        self.protocols = {'xiaomi': XiaomiTransport, 'ninebot': NinebotTransport}
+        self.device = ''
+        self.fwfilep = ''
+        self.interface = 'bleandroid'
+        self.protocol = 'ninebot'
+        self.address = ''
 
-	def setaddr(self, a):
-		self.address = a
-		print(self.address+' selected as address')
+    def setaddr(self, a):
+        self.address = a
+        print(self.address+' selected as address')
 
-	def setdev(self, d):
-		self.device = d
-		print(self.device+' selected as device')
+    def setdev(self, d):
+        self.device = d
+        print(self.device+' selected as device')
 
-	def setfwfilep(self, f):
-		self.fwfilep = f
-		print(self.fwfilep+' selected as fwfile')
+    def setfwfilep(self, f):
+        self.fwfilep = f
+        print(self.fwfilep+' selected as fwfile')
 
-	def setinterface(self, i):
-		self.interface = i
-		print(self.interface+' selected as interface')
+    def setiface(self, i):
+        self.interface = i
+        print(self.interface+' selected as interface')
 
-	def setproto(self, p):
-		self.protocol = p
-		print(self.protocol+' selected as protocol')
+    def setproto(self, p):
+        self.protocol = p
+        print(self.protocol+' selected as protocol')
 
-	def checksum(s, data):
-		for c in data:
-			s += ord(c)
-		return (s & 0xFFFFFFFF)
+    def checksum(s, data):
+        for c in data:
+            s += ord(c)
+        return (s & 0xFFFFFFFF)
 
-	def UpdateFirmware(link, tran, dev, fwfile):
-		print('flashing '+self.fwfilep+' to '+ self.device)
-		fwfile.seek(0, os.SEEK_END)
-		fw_size = fwfile.tell()
-		fwfile.seek(0)
-		fw_page_size = 0x80
+    def UpdateFirmware(self, link, tran, dev, fwfile):
+        print('flashing '+self.fwfilep+' to ' + self.device)
+        fwfile.seek(0, os.SEEK_END)
+        fw_size = fwfile.tell()
+        fwfile.seek(0)
+        fw_page_size = 0x80
 
-		dev = self.devices.get(self.device)
-		print('Pinging...')
-		for retry in range(self.PING_RETRIES):
-			print('.')
-			try:
-				if dev==BT.BLE:
-					tran.execute(ReadRegs(dev, 0, '13s'))
-				else:
-					tran.execute(ReadRegs(dev, 0x10, '14s'))
-			except LinkTimeoutException:
-				continue
-			break
-		else:
-			print('Timed out !')
-			return False
-		print('OK')
+        dev = self.devices.get(self.device)
+        print('Pinging...')
+        for retry in range(self.PING_RETRIES):
+            print('.')
+            try:
+                if dev == BT.BLE:
+                    tran.execute(ReadRegs(dev, 0, '13s'))
+                else:
+                    tran.execute(ReadRegs(dev, 0x10, '14s'))
+            except LinkTimeoutException:
+                continue
+            break
+        else:
+            print('Timed out !')
+            return False
+        print('OK')
 
-		if self.interface!='tcpnl':
-			print('Locking...')
-			tran.execute(WriteRegs(BT.ESC, 0x70, '<H', 0x0001))
-		else:
-			print('Not Locking...')
+        if self.interface != 'tcpnl':
+            print('Locking...')
+            tran.execute(WriteRegs(BT.ESC, 0x70, '<H', 0x0001))
+        else:
+            print('Not Locking...')
 
-		print('Starting...')
-		tran.execute(StartUpdate(dev, fw_size))
+        print('Starting...')
+        tran.execute(StartUpdate(dev, fw_size))
 
-		print('Writing...')
-		page = 0
-		chk = 0
-		while fw_size:
-			chunk_sz = min(fw_size, fw_page_size)
-			data = fwfile.read(chunk_sz)
-			chk = checksum(chk, data)
-			#tran.execute(WriteUpdate(dev, page, data))
-			tran.execute(WriteUpdate(dev, page, data+b'\x00'*(fw_page_size-chunk_sz))) # TODO: Ninebot wants this padding. Will it work on M365 too?
-			page += 1
-			fw_size -= chunk_sz
+        print('Writing...')
+        page = 0
+        chk = 0
+        while fw_size:
+            chunk_sz = min(fw_size, fw_page_size)
+            data = fwfile.read(chunk_sz)
+            chk = checksum(chk, data)
+            #tran.execute(WriteUpdate(dev, page, data))
+            tran.execute(WriteUpdate(dev, page, data+b'\x00'*(fw_page_size-chunk_sz)))
+            page += 1
+            fw_size -= chunk_sz
 
-		print('Finalizing...')
-		tran.execute(FinishUpdate(dev, chk ^ 0xFFFFFFFF))
+        print('Finalizing...')
+        tran.execute(FinishUpdate(dev, chk ^ 0xFFFFFFFF))
 
-		print('Reboot')
-		tran.execute(RebootUpdate(dev))
-		print('Done')
-		return True
+        print('Reboot')
+        tran.execute(RebootUpdate(dev))
+        print('Done')
+        return True
 
-	def Flash(self, fwfilep):
-		if self.device=='extbms' and self.protocol!='ninebot':
-			exit('Only Ninebot supports External BMS !')
-		setfwfilep(fwfilep)
-		file = open(self.fwfilep, 'rb')
-		dev = self.devices.get(self.device)
-		if self.interface=='bleandroid':
-			try:
-				from py9b.link.bleandroid import BLELink
-			except:
-				exit('BLE is not supported on your system !')
-			link = BLELink()
-		elif self.interface=='tcp':
-			from py9b.link.tcp import TCPLink
-			link = TCPLink()
-		elif self.interface=='serial':
-			from py9b.link.serial import SerialLink
-			link = SerialLink()
-		else:
-			exit('!!! BUG !!! Unknown self.interface selected: '+self.interface)
+    def Flash(self, fwfilep):
+        if self.device == 'extbms' and self.protocol != 'ninebot':
+            exit('Only Ninebot supports External BMS !')
+        setfwfilep(fwfilep)
+        file = open(self.fwfilep, 'rb')
+        dev = self.devices.get(self.device)
+        if self.interface == 'bleandroid':
+            try:
+                from py9b.link.bleandroid import BLELink
+            except:
+                exit('BLE is not supported on your system !')
+            link = BLELink()
+        elif self.interface == 'tcp':
+            from py9b.link.tcp import TCPLink
+            link = TCPLink()
+        elif self.interface == 'serial':
+            from py9b.link.serial import SerialLink
+            link = SerialLink()
+        else:
+            exit('!!! BUG !!! Unknown self.interface selected: '+self.interface)
 
-		with link:
-			tran = self.protocols.get(self.protocol)(link)
+        with link:
+            tran = self.protocols.get(self.protocol)(link)
 
-			if self.address!='':
-				addr = self.address
-			elif self.interface!='bleandroid':
-				print('Scanning...')
-				ports = link.scan()
-				if not ports:
-					exit("No self.interfaces found !")
-				print('Connecting to', ports[0][0])
-				addr = ports[0][1]
-			else:
-				raise LinkOpenException
+            if self.address != '':
+                addr = self.address
+            elif self.interface != 'bleandroid':
+                print('Scanning...')
+                ports = link.scan()
+                if not ports:
+                    exit("No self.interfaces found !")
+                print('Connecting to', ports[0][0])
+                addr = ports[0][1]
+            else:
+                raise LinkOpenException
 
-			link.open(addr)
-			print('Connected')
-			try:
-				UpdateFirmware(link, tran, dev, file)
-			except Exception as e:
-				print('Error:', e)
-				raise
+            link.open(addr)
+            print('Connected')
+            try:
+                UpdateFirmware(link, tran, dev, file)
+            except Exception as e:
+                print('Error:', e)
+                raise
--- main.py
+++ main.py
@@ -8,81 +8,88 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.progressbar import ProgressBar
+#from kivy.uix.progressbar import ProgressBar
 from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.textinput import TextInput
-
-from .fwupd import FWUpd as fwupd
+from kivy.uix.spinner import Spinner
+from fwupd import FWUpd as fwupd
 
 
 class NineRiFt(App):
 
-	def build(self):
-		root_folder = getattr(self, 'user_data_dir')
-		cache_folder = os.path.join(root_folder, 'cache')
+    def build(self):
+        root_folder = getattr(self, 'user_data_dir')
+        cache_folder = os.path.join(root_folder, 'cache')
 
-		title_label = Label(text="NineRiFt", font_size='12sp',
-		 size_hint_x=1, height='16sp')
+        title_label = Label(text="NineRiFt", font_size='12sp',
+         size_hint_x=1, height='16sp')
 
-		seladdr_label = Label(text="Addr:", font_size='12sp', height='14sp',
-		 size_hint_y=1, size_hint_x=.08)
+        seladdr_label = Label(text="Addr:", font_size='12sp', height='14sp',
+         size_hint_y=1, size_hint_x=.08)
 
-		seladdr_input = TextInput(multiline=False, text='',
-		height='12sp', font_size='12sp', size_hint_x=.92, size_hint_y=1)
+        seladdr_input = TextInput(multiline=False, text='',
+        height='12sp', font_size='12sp', size_hint_x=.92, size_hint_y=1)
 
-		seladdr_input.bind(on_text_validate=lambda x: fwupd.setdev(seladdr_input.text))
+        seladdr_input.bind(on_text_validate=lambda x: fwupd.setdev(seladdr_input.text))
 
-		selfile_label = Label(text="FW file:", font_size='12sp', size_hint_x=1, height='12sp')
+        selfile_label = Label(text="FW file:", font_size='12sp', size_hint_x=1, height='12sp')
 
-		ble_button = Button(text="BLE", font_size='12sp', height='15sp',
-		 on_press=lambda x:fwupd.setdev('ble'))
+        ifaceselspin= Spinner(text='TCP', values=('TCP', 'Serial', 'BLE'), font_size='12sp', height='15sp', on_text=lambda x:fwupd.setiface(ifaceselspin.text.lower))
 
-		esc_button = Button(text="ESC", font_size='12sp', height='15sp',
-		 on_press=lambda x:fwupd.setdev('esc'))
+        devselspin= Spinner(text='ESC', values=('BLE', 'ESC', 'BMS', 'ExtBMS'), font_size='12sp', height='15sp', on_text=lambda x:fwupd.setdev(devselspin.text.lower))
 
-		bms_button = Button(text="BMS", font_size='12sp', height='15sp',
-		 on_press=lambda x:fwupd.setdev('bms'))
+        # ble_button = Button(text="BLE", font_size='12sp', height='15sp',
+        #  on_press=lambda x:fwupd.setdev('ble'))
+        #
+        # esc_button = Button(text="ESC", font_size='12sp', height='15sp',
+        #  on_press=lambda x:fwupd.setdev('esc'))
+        #
+        # bms_button = Button(text="BMS", font_size='12sp', height='15sp',
+        #  on_press=lambda x:fwupd.setdev('bms'))
+        #
+        # ebms_button = Button(text="EBMS", font_size='12sp', height='15sp',
+        #  on_press=lambda x:fwupd.setdev('extbms'))
 
-		ebms_button = Button(text="EBMS", font_size='12sp', height='15sp',
-		 on_press=lambda x:fwupd.setdev('extbms'))
+        selfile = FileChooserListView(path=cache_folder)
 
-		selfile = FileChooserListView(path=cache_folder)
+        flash_button = Button(text="Flash", font_size='15sp', height='16sp',
+         on_press=lambda x: fwupd.Flash(selfile.selection[0]))
 
-		flash_button = Button(text="Flash", font_size='15sp', height='16sp',
-		 on_press=lambda x:fwupd.Flash(selfile.selection[0]))
+        titlelayout = BoxLayout(orientation='vertical', size_hint_y=.15)
+        titlelayout.add_widget(title_label)
 
-		titlelayout = BoxLayout(orientation='vertical', size_hint_y=.15)
-		titlelayout.add_widget(title_label)
+        toplayout = GridLayout(rows=2, size_hint_y=.2)
+        addrlayout = BoxLayout(orientation='horizontal', size_hint_y=.3)
+        addrlayout.add_widget(seladdr_label)
+        addrlayout.add_widget(seladdr_input)
+        topbtnlayout = BoxLayout(orientation='horizontal', size_hint_y=.7)
+        # topbtnlayout.add_widget(ble_button)
+        # topbtnlayout.add_widget(esc_button)
+        # topbtnlayout.add_widget(bms_button)
+        # topbtnlayout.add_widget(ebms_button)
+        topbtnlayout.add_widget(ifaceselspin)
+        topbtnlayout.add_widget(devselspin)
+        toplayout.add_widget(addrlayout)
+        toplayout.add_widget(topbtnlayout)
 
-		toplayout = GridLayout(rows=2, size_hint_y=.2)
-		addrlayout = BoxLayout(orientation='horizontal', size_hint_y=.3)
-		addrlayout.add_widget(seladdr_label)
-		addrlayout.add_widget(seladdr_input)
-		topbtnlayout = BoxLayout(orientation='horizontal', size_hint_y=.7)
-		topbtnlayout.add_widget(ble_button)
-		topbtnlayout.add_widget(esc_button)
-		topbtnlayout.add_widget(bms_button)
-		topbtnlayout.add_widget(ebms_button)
-		toplayout.add_widget(addrlayout)
-		toplayout.add_widget(topbtnlayout)
+        midlayout = BoxLayout(orientation='vertical', size_hint_y=.70)
+        midlabelbox = AnchorLayout(anchor_y='top', size_hint_y=.1)
+        midlabelbox.add_widget(selfile_label)
+        midlayout.add_widget(midlabelbox)
+        midlayout.add_widget(selfile)
 
-		midlayout = BoxLayout(orientation='vertical', size_hint_y=.85)
-		midlabelbox = AnchorLayout(anchor_y='top', size_hint_y=.1)
-		midlabelbox.add_widget(selfile_label)
-		midlayout.add_widget(midlabelbox)
-		midlayout.add_widget(selfile)
+        botlayout = BoxLayout(orientation='vertical', size_hint_y=.15)
+        botlayout.add_widget(flash_button)
+        flash_button = Button(text="Flash", font_size='18sp', height='20sp')
+        #botlayout.add_widget(pb)
 
-		botlayout = BoxLayout(orientation='vertical', size_hint_y=.2)
-		botlayout.add_widget(flash_button)
-		flash_button = Button(text="Flash", font_size='18sp', height='20sp')
-		#botlayout.add_widget(pb)
+        mainlayout = GridLayout(cols=1, rows=4)
+        mainlayout.add_widget(titlelayout)
+        mainlayout.add_widget(toplayout)
+        mainlayout.add_widget(midlayout)
+        mainlayout.add_widget(botlayout)
+        return mainlayout
 
-		mainlayout = GridLayout(cols=1, rows=4)
-		mainlayout.add_widget(titlelayout)
-		mainlayout.add_widget(toplayout)
-		mainlayout.add_widget(midlayout)
-		mainlayout.add_widget(botlayout)
-		return mainlayout
 
 if __name__ == "__main__":
-	NineRiFt().run()
+    NineRiFt().run()
