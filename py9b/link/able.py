475323dc7dac732a4f0879cc12be6311893f1c22
Slinger360 <dilsha21@gmail.com>
2019-05-21 14:35:47 -0500

    fixed IndentErrors

--- py9b/link/able.py
+++ py9b/link/able.py
@@ -34,88 +34,88 @@ class BLELink(BaseLink, BluetoothDispatcher):
 		self.ble_device = ObjectProperty(None)
 
 	identity = bytearray([
-        0x4e, 0x42  # Ninebot Bluetooth ID
+	0x4e, 0x42  # Ninebot Bluetooth ID
     ])
 
-    service_ids = {
-        'retail': '6e400001-b5a3-f393-e0a9-e50e24dcca9e' #service UUID
+	service_ids = {
+	'retail': '6e400001-b5a3-f393-e0a9-e50e24dcca9e' #service UUID
     }
 
-    receive_ids = {
-        'retail': '6e400002-b5a3-f393-e0a9-e50e24dcca9e' #receive characteristic UUID
+	receive_ids = {
+    'retail': '6e400002-b5a3-f393-e0a9-e50e24dcca9e' #receive characteristic UUID
     }
 
-    transmit_ids = {
-        'retail': '6e400003-b5a3-f393-e0a9-e50e24dcca9e' #transmit characteristic UUID
+	transmit_ids = {
+    'retail': '6e400003-b5a3-f393-e0a9-e50e24dcca9e' #transmit characteristic UUID
     }
 
 
-    def discover(self):
-        self.start_scan()
-        self.state = 'scan'
+	def discover(self):
+		self.start_scan()
+		self.state = 'scan'
 
 
-    def on_device(self, device, rssi, advertisement):
-        if self.state != 'scan':
-            return
-        Logger.debug("on_device event {}".format(list(advertisement)))
-        scoot_found = False
-        address = device.getAddress()
-        if self.addr and address.startswith(self.addr):  # is a Mi Band device
-            self.ble_device = device
-            scoot_found = True
-            self.stop_scan()
-        else:
-            for ad in advertisement:
-                if ad.ad_type == Advertisement.ad_types.manufacturer_specific_data:
-                    if ad.data.startswith(self.identity):
-                        scoot_found = True
-                    else:
-                        break
-                elif ad.ad_type == Advertisement.ad_types.complete_local_name:
-                    name = str(ad.data)
+	def on_device(self, device, rssi, advertisement):
+		if self.state != 'scan':
+			return
+		Logger.debug("on_device event {}".format(list(advertisement)))
+		scoot_found = False
+		address = device.getAddress()
+		if self.addr and address.startswith(self.addr):  # is a Mi Band device
+			self.ble_device = device
+			scoot_found = True
+			self.stop_scan()
+		else:
+			for ad in advertisement:
+				if ad.ad_type == Advertisement.ad_types.manufacturer_specific_data:
+					if ad.data.startswith(self.identity):
+						scoot_found = True
+					else:
+						break
+				elif ad.ad_type == Advertisement.ad_types.complete_local_name:
+					name = str(ad.data)
         if scoot_found:
             self.state = 'found'
             self.ble_device = device
             Logger.debug("Scooter detected: {}".format(name))
             self.stop_scan()
 
-    def on_scan_completed(self):
-        if self.ble_device:
-            self.connect_gatt(self.ble_device)
-        else:
-            self.start_scan()
+	def on_scan_completed(self):
+		if self.ble_device:
+			self.connect_gatt(self.ble_device)
+		else:
+			self.start_scan()
 
-    def on_connection_state_change(self, status, state):
-        if status == GATT_SUCCESS and state:
-            self.discover_services()
-        else:
-            self.close_gatt()
-            self.services = None
+	def on_connection_state_change(self, status, state):
+		if status == GATT_SUCCESS and state:
+			self.discover_services()
+		else:
+			self.close_gatt()
+			self.services = None
 
-    def on_services(self, status, services):
-        self.services = services
-        for uuid in receive_ids.values():
-            self.rx_characteristic = self.services.search(uuid)
-        for uuid in transmit_ids.values():
-            self.tx_characteristic = self.services.search(uuid)
-            self.enable_notifications(self.tx_characteristic)
+	def on_services(self, status, services):
+		self.services = services
+		for uuid in receive_ids.values():
+			self.rx_characteristic = self.services.search(uuid)
+		for uuid in transmit_ids.values():
+			self.tx_characteristic = self.services.search(uuid)
+			self.enable_notifications(self.tx_characteristic)
 
-    def on_characteristic_changed(self, characteristic):
-        if characteristic == self.tx_characteristic:
-            data = characteristic.getValue()
-            self.rx_fifo.write(data)
+	def on_characteristic_changed(self, characteristic):
+		if characteristic == self.tx_characteristic:
+			data = characteristic.getValue()
+			self.rx_fifo.write(data)
 
-    def open(self, port):
-        self.tx_characteristic = self.rx_characteristic = None
-        self.addr = port
-        discover()
+	def open(self, port):
+		self.tx_characteristic = self.rx_characteristic = None
+		self.addr = port
+		discover()
 
-    def close(self):
-        self.close_gatt()
-        self.services = None
+	def close(self):
+		self.close_gatt()
+		self.services = None
 
-    def read(self, size):
+	def read(self, size):
 		try:
 			data = self.rx_fifo.read(size, timeout=self.timeout)
 		except queue.Empty:
@@ -124,12 +124,12 @@ class BLELink(BaseLink, BluetoothDispatcher):
 			print '<', hexlify(data).upper()
 		return data
 
-    def write(self, data):
-        if self.dump:
+	def write(self, data):
+		if self.dump:
 			print '>', hexlify(data).upper()
-        size = len(data)
-        ofs = 0
-        while size:
+		size = len(data)
+		ofs = 0
+		while size:
 			chunk_sz = min(size, _write_chunk_size)
 			self.write_characteristic(rx_characteristic, bytearray(data[ofs:ofs+chunk_sz]))
 			ofs += chunk_sz
