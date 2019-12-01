16c2da3be419f0be5b3acb21107566907308e413
Slinger360 <slinger360@users.noreply.github.com>
2019-12-01 14:43:48 -0600

    adjusted UI and added max to flash screen (not ready yet)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.core.window import Window
 from kivy.clock import Clock, mainthread
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -126,6 +127,9 @@ class NineRiFt(App):
         if mod is 'esx':
             self.versel = False
             self.hasextbms = True
+        if mod is 'max':
+            self.versel = False
+            self.hasextbms = False
         if self.hasextbms is True:
             try:
                 values.append('ExtBMS')
